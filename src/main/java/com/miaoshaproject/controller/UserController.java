f2cf45711c03f7f2d276757ac54dd810b68a30a2
test <test@test.com>
2020-04-28 00:13:49 +0800

    12-15

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -7,21 +7,70 @@ import com.miaoshaproject.response.CommonReturnType;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
 import org.apache.catalina.User;
-import org.apache.ibatis.ognl.ObjectElementsAccessor;
+import org.apache.tomcat.util.security.MD5Encoder;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
 
 @Controller("user")
 @RequestMapping("/user")
-public class UserController {
+@CrossOrigin
+public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户注册接口
+    @RequestMapping(value = "/register", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType register(@RequestParam(name="telphone")String telphone,
+                                     @RequestParam(name="otpCode")String otpCode,
+                                     @RequestParam(name="name")String name,
+                                     @RequestParam(name="gender")Byte gender,
+                                     @RequestParam(name="password")String password,
+                                     @RequestParam(name="age")Byte age) throws BusinessException {
+        //验证手机号和对应的otpcode相符合
+        String inSessionOtpCode = (String) this.httpServletRequest.getSession().getAttribute(telphone);
+        if(!com.alibaba.druid.util.StringUtils.equals(otpCode,inSessionOtpCode)){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR.setErrMsg("短信验证码错误"));
+        }
+        //用户注册流程
+        UserModel userModel = new UserModel();
+        userModel.setName(name);
+        userModel.setGender(gender);
+        userModel.setAge(age);
+        userModel.setTelphone(telphone);
+        userModel.setRegisterMode("byphone");
+        userModel.setEncryptPassword(MD5Encoder.encode(password.getBytes()));
+
+        userService.register(userModel);
+        return CommonReturnType.create(null)
+    }
+
+    //用户获取otp短信接口
+    @RequestMapping(value = "/getotp", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name="telphone")String telphone) {
+        //需要按照一定的规则生成OTP验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt+=10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将OTP验证码同对应用户的手机号关联，使用httpsession的方式绑定他的手机号和OTPCODE
+        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+
+        //将OTP验证码通过短信通道发送给用户，省略
+        System.out.println("telphone = " + telphone + " & otpCode = " + otpCode);
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
@@ -31,7 +80,8 @@ public class UserController {
 
         //若获取的对应用户信息不存在
         if(userModel == null) {
-            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+            userModel.setEncryptPassword("123");
+//            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
         }
 
         //将核心领域模型用户对象转换为可供UI使用的viewobject
@@ -50,14 +100,4 @@ public class UserController {
         return userVO;
     }
 
-    //定义exceptionHandler解决未被controller层吸收的exception
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Object handlerException(HttpServletRequest request, Exception ex) {
-        CommonReturnType commonReturnType = new CommonReturnType();
-        commonReturnType.setStatus("fail");
-        commonReturnType.setData(ex);
-        return commonReturnType;
-    }
 }
