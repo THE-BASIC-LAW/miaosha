025e534cbb3d9ad6468740e9d517c1dbb537d751
root <root@DESKTOP-4H0N9U2.localdomain>
2020-04-27 19:57:38 +0800

    12-09

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.miaoshaproject.controller;
 
 import com.miaoshaproject.controller.viewobject.UserVO;
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
+import com.miaoshaproject.response.CommonReturnType;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
 import org.apache.catalina.User;
+import org.apache.ibatis.ognl.ObjectElementsAccessor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller("user")
 @RequestMapping("/user")
@@ -20,12 +25,20 @@ public class UserController {
 
     @RequestMapping("/get")
     @ResponseBody
-    public UserVO getUer(@RequestParam(name="id") Integer id) {
+    public CommonReturnType getUer(@RequestParam(name="id") Integer id) throws BusinessException{
         //调用service服务获取对应id的用户对象返回给前端
         UserModel userModel = userService.getUserById(id);
 
+        //若获取的对应用户信息不存在
+        if(userModel == null) {
+            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
+
         //将核心领域模型用户对象转换为可供UI使用的viewobject
-        return convertFromModel(userModel);
+        UserVO userVO = convertFromModel(userModel);
+
+        //返回通用对象
+        return CommonReturnType.create(userVO);
     }
 
     private UserVO convertFromModel(UserModel userModel) {
@@ -35,5 +48,16 @@ public class UserController {
         UserVO userVO = new UserVO();
         BeanUtils.copyProperties(userModel,userVO);
         return userVO;
+    }
+
+    //定义exceptionHandler解决未被controller层吸收的exception
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Object handlerException(HttpServletRequest request, Exception ex) {
+        CommonReturnType commonReturnType = new CommonReturnType();
+        commonReturnType.setStatus("fail");
+        commonReturnType.setData(ex);
+        return commonReturnType;
     }
 }
